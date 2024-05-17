591e4d2c493c48568ddc3b8855d8bd2f9fc1ae81
Prasadnair <prasadnair44@gmail.com>
2024-05-16 21:25:05 -0400

    new data annotations in dotnet8

---
+++ NewDataAnnotationsInDotNet8/NewDataAnnotationsInDotNet8/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewDataAnnotationsInDotNet8
+{
+    public class Item
+    {
+        [Length(5, 20)]
+        public string Name { get; set; }
+
+        [Range(250, 2000, MinimumIsExclusive = true, MaximumIsExclusive = false)]
+        public double Price { get; set; }
+
+        [AllowedValues("Phones","Tabs")]
+        public string Category { get; set; }
+
+        [DeniedValues("Monitors")]
+        public string SubCategory { get; set; }
+
+        [Base64String]
+        public string Description { get; set; }
+    }
+}
---
+++ NewDataAnnotationsInDotNet8/NewDataAnnotationsInDotNet8/PlaceOrderEndPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewDataAnnotationsInDotNet8
+{
+    public static class PlaceOrderEndPoint
+    {
+        public static void AddPlaceOrderEndPoint(this IEndpointRouteBuilder app)
+        {
+
+            var placeOrderEndPoint = app.MapGroup("api/v1");
+            placeOrderEndPoint.PlaceOrdrEndPoint();
+        }
+
+        private static void PlaceOrdrEndPoint(this RouteGroupBuilder routeGroup)
+        {
+            routeGroup.MapPost("/placeorder", async (Item item) =>
+            {
+                var validationResults = new List<ValidationResult>();
+                var validationContext = new ValidationContext(item, null, null);
+                bool isValid = Validator.TryValidateObject(item, validationContext, validationResults, true);
+
+                if (!isValid)
+                {
+                    return Results.BadRequest(validationResults);
+                }
+                // Place order logic
+                return Results.Created("/order/1", item);
+            })
+            .WithName("PlaceOrder")
+            .WithOpenApi();
+
+        }
+    }
+}
